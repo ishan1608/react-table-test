f0f5037d79e07eb5e67473e86fa3e041a09efafa
Ishan <ishan1608@gmail.com>
2019-03-06 18:20:37 +0530

    added sample table of react-table in root component

--- src/components/root/root_component.js
+++ src/components/root/root_component.js
@@ -5,26 +5,32 @@ import {hot} from "react-hot-loader/root";
 
 import LikeCounterComponent from "../like_counter/like_counter_component";
 import WebpackLogo from './webpack.svg';
+import ReactTableSimpleTableComponent from '../react-table-simple-table/react_table_simple_table_component';
 
 
 setConfig({
-	pureRender: true, // RHL will not change render method
+    pureRender: true, // RHL will not change render method
 });
 
 class RootComponent extends React.Component {
 
-	render() {
-		console.log('%c RootComponent.render()', 'color: green');
-		return <div className="root-component">
-			<img className="webpack-logo" src={WebpackLogo}/>
-			<p>Hello, {this.props.name}</p>
-			<LikeCounterComponent/>
-		</div>;
-	}
+    render() {
+        console.log('%c RootComponent.render()', 'color: green');
+        return (
+            <div>
+                <div className="root-component">
+                    <img className="webpack-logo" src={WebpackLogo}/>
+                    <p>Hello, {this.props.name}</p>
+                    <LikeCounterComponent/>
+                </div>
+                <ReactTableSimpleTableComponent/>
+            </div>
+        );
+    }
 
-	componentWillUnmount() {
-		console.log('%c RootComponent.componentWillUnmount()', 'color: red');
-	}
+    componentWillUnmount() {
+        console.log('%c RootComponent.componentWillUnmount()', 'color: red');
+    }
 
 }
 
